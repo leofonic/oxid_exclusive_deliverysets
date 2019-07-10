2333dbb83a42e3067ccff64e767b510acea11835
Frank Zunderer <info@zunderweb.de>
2019-07-10 15:15:19 +0200

    Update module_options.php

--- 4.7-4.10/copy_this/modules/zunderweb/exclusive_deliverysets/views/admin/en/module_options.php
+++ 4.7-4.10/copy_this/modules/zunderweb/exclusive_deliverysets/views/admin/en/module_options.php
@@ -5,4 +5,5 @@ $aLang = array(
 'charset'                                       => 'ISO-8859-15',
 'SHOP_MODULE_GROUP_exclusive_deliverysets_main'                         => 'Settings',
 'SHOP_MODULE_aExclusiveDeliverysets'            => 'IDs of Shipping Sets that will appear exclusive',
+'SHOP_MODULE_aImportantDeliverysets'            => 'IDs of Shipping Sets that will not be hidden',
 );
